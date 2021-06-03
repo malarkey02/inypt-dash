816560c0c708f190aca8ca474dcb9767d44449fa
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-03 16:25:31 +0530

    implement hooks

--- src/components/internalComp/task-list-entry.jsx
+++ src/components/internalComp/task-list-entry.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/internal-styles/task-list-entry.css";
 
 function Taskentry(props) {
+  const [taskName, setTaskName] = useState(props.task);
+  const [taskOwner, setTaskOwner] = useState(props.owner);
+  const [taskDue, setTaskDue] = useState(props.due);
+
   return (
     <div className="task-entry">
       <div
         className="banner-color"
         style={{ backgroundColor: props.banner }}
       ></div>
-      <div className="task-name">{props.task}</div>
-      <div className="task-owner">{props.owner}</div>
-      <div className="task-due">{props.due}</div>
+      <div className="task-name">{taskName}</div>
+      <div className="task-owner">{taskOwner}</div>
+      <div className="task-due">{taskDue}</div>
       <input type="checkbox" name="task-done" id="task-list-check" />
     </div>
   );
