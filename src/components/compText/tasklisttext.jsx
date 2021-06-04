9dfc2072d25e0ca4d6168a6a798a99368454b1b3
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-04 17:23:58 +0530

    headers created, first commit

---
+++ src/components/compText/tasklisttext.jsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import "../../styles/internal-text-styles/tasklisttext.css";
+
+function TaskEntryHead() {
+  return (
+    <div className="listHeadder">
+      Task List
+      <div className="listSubHead-container">
+        <div className="Task">Task</div>
+        <div className="Owner">Owner</div>
+        <div className="Due">Due</div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskEntryHead;
