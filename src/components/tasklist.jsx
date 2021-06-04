319a9fdb948d3b4560e980cb711b6197376a7cb8
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-04 17:23:43 +0530

    list headders added

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import "../styles/tasklist.css";
 import Taskentry from "./internalComp/task-list-entry.jsx";
+import TaskEntryHead from "./compText/tasklisttext.jsx";
 
 function Tasklist() {
   return (
-    <div className="tasklist">
-      <Taskentry
-        banner="#FF8289"
-        task="Qual. Theory Eq. 4"
-        owner="Admin"
-        due="25/4"
-      />
-      <Taskentry
-        banner="#FA3B49"
-        task="Qual. Theory Eq. 5"
-        owner="Admin"
-        due="29/4"
-      />
+    <div className="list-container">
+      <div className="tasklist">
+        <TaskEntryHead />
+        <Taskentry
+          banner="#FF8289"
+          task="Qual. Theory Eq. 4"
+          owner="Admin"
+          due="25/4"
+        />
+        <Taskentry
+          banner="#FA3B49"
+          task="Qual. Theory Eq. 5"
+          owner="Admin"
+          due="29/4"
+        />
+      </div>
     </div>
   ); //pass props... somehow
 }
