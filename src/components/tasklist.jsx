cf6dcc8079b6d807163403d4c02b7ae4d80ef6c8
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-04 16:41:15 +0530

    multi component test

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -11,6 +11,12 @@ function Tasklist() {
         owner="Admin"
         due="25/4"
       />
+      <Taskentry
+        banner="#FA3B49"
+        task="Qual. Theory Eq. 5"
+        owner="Admin"
+        due="29/4"
+      />
     </div>
   ); //pass props... somehow
 }
