1b86f1540bf8f32d8bc988c85fd56a61ed8faeaf
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-04 17:37:38 +0530

    color key div headder ,first commit

---
+++ src/components/compText/taskcolorkeytext.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/internal-text-styles/task-colorkey-text.css";
+
+function KeysText() {
+  return <div className="color-listHeadder">Task Color Key</div>;
+}
+
+export default KeysText;
