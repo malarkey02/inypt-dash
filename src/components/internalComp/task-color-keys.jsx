a7908ac3d0346f8041f0f2f029ebdd81f525a255
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-04 16:41:39 +0530

    map prop obj to disp data

---
+++ src/components/internalComp/task-color-keys.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../styles/internal-styles/task-color-keys.css";
+
+function Colorkeys(props) {
+  const taskLegends = props.taskLegends;
+
+  return (
+    <div className="keyEntry">
+      {taskLegends.map((taskLegend) => (
+        <div>
+          <span
+            className="dot"
+            style={{ backgroundColor: taskLegend.color }}
+          ></span>
+
+          <span className="taskName"> ~ {taskLegend.task}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Colorkeys;
