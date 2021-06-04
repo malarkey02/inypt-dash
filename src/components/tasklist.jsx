93903180ac84c88647a5e27469e514aafd48c6de
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-05 00:42:12 +0530

    render as objects with useState

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/tasklist.css";
 import Taskentry from "./internalComp/task-list-entry.jsx";
 import TaskEntryHead from "./compText/tasklisttext.jsx";
+import Addentry from "./internalComp/add-task-entry.jsx";
 
 function Tasklist() {
+  const [entries, setEntries] = useState([
+    {
+      banner: "#FF8289",
+      task: "Qual. Theory Eq. 4",
+      owner: "Admin",
+      due: "25/4",
+    },
+    {
+      banner: "#FA3B49",
+      task: "Qual. Theory Eq. 5",
+      owner: "Admin",
+      due: "29/4",
+    },
+  ]);
+
   return (
     <div className="list-container">
       <div className="tasklist">
         <TaskEntryHead />
-        <Taskentry
-          banner="#FF8289"
-          task="Qual. Theory Eq. 4"
-          owner="Admin"
-          due="25/4"
-        />
-        <Taskentry
-          banner="#FA3B49"
-          task="Qual. Theory Eq. 5"
-          owner="Admin"
-          due="29/4"
-        />
+        {entries.map((entry) => (
+          <Taskentry
+            banner={entry.banner}
+            task={entry.task}
+            owner={entry.owner}
+            due={entry.due}
+          />
+        ))}
+        <Addentry />
       </div>
     </div>
   ); //pass props... somehow
