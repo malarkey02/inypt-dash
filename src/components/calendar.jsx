050811eace26553bb45e59bc7213589ff145f3a6
Anupam Dash <anupam.dash0505@gmail.com>
2021-06-08 17:29:11 +0530

    added calendar

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import '../styles/calendar.css';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "../styles/calendar.css";
 
-function Calendar(){
-    return <div className="calendar">
+function _Calendar() {
+  function titleClassName({ dat, view }) {
+    if (view === "month") {
+      if (datesToAddClassTo.find((dDate) => isSameDay(dDate, date))) {
+        return " ";
+      }
+    }
+  }
 
+  return (
+    <div className="calendar-container">
+      <div>
+        <Calendar />
+      </div>
     </div>
+  );
 }
 
-export default Calendar; 
+export default _Calendar;
