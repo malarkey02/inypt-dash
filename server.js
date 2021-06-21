398ded2509c4450a8d4f9c2335b3176071638c14
Harsh Mathur <harshmathur2002@gmail.com>
2021-06-21 16:32:54 +0530

    Adding security to Database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json({limit:'1mb'}));
 try {
-    mongoose.connect('mongodb+srv://admin-harsh:7hoPUeNHT2b42upX@cluster0.8y5it.mongodb.net/InYPTUsers?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect("mongodb+srv://admin-harsh:"+ process.env.DB_PASS+ "@cluster0.8y5it.mongodb.net/InYPTUsers?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 } catch (err){
     console.log(err); 
