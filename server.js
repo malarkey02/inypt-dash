12d66002bc0f4b790e2791317cf387afa6d29cf0
Anupam Dash <anupam.dash0505@gmail.com>
2021-08-02 01:58:36 +0530

    prettified

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3002",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:3002",
+//     methods: ["GET", "POST"],
+//   },
+// });
 require("dotenv").config();
 
 const app = express();
+// const server = http.createServer(app);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -147,25 +147,25 @@ app.use("/getprojects", function (req, res) {
 });
 
 //rtc sockets
-io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
+// io.on("connection", (socket) => {
+//   socket.emit("me", socket.id);
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("callEnded");
+//   });
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
-  });
+//   socket.on("callUser", (data) => {
+//     io.to(data.userToCall).emit("callUser", {
+//       signal: data.signalData,
+//       from: data.from,
+//       name: data.name,
+//     });
+//   });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted"), data.signal;
-  });
-});
+//   socket.on("answerCall", (data) => {
+//     io.to(data.to).emit("callAccepted"), data.signal;
+//   });
+// });
 
 app.listen(3002, function (req, res) {
   console.log("Listening at 3002");
--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,50 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import React, {useState} from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
 
-
-import '../styles/App.css';
-import Dashboard from './Dashboard'; 
-import Problem5 from './internalComp/ProjectPages/problem5'
-import Login from './login/Login';
-import useToken from './login/useToken';
-
-
-
-
-
-
+import "../styles/App.css";
+import Dashboard from "./Dashboard";
+import Problem5 from "./internalComp/ProjectPages/problem5";
+import Login from "./login/Login";
+import useToken from "./login/useToken";
+import Meetings from "./videoChat/meetingWin";
+import Onboard from "./onboarding/onboard";
 
 function App() {
+  const { token, setToken } = useToken();
 
-  const {token, setToken} = useToken(); 
-  
-  if(!token) {
-    return <div className="center-align">
-      <Login setToken={setToken}/> 
-    </div>
-  } 
-
+  if (!token) {
+    return (
+      <div className="center-align">
+        <Login setToken={setToken} />
+      </div>
+    );
+  }
 
   return (
     <Router>
-        <Switch>
-          <Route exact path="/dashboard">
-            <Dashboard userid={token}/> 
-          </Route>
+      <Switch>
+        <Route exact path="/dashboard">
+          <Dashboard userid={token} />
+        </Route>
 
-          <Route exact path="/problem-14">
-            <Problem5/>
-          </Route>
+        <Route exact path="/problem-14">
+          <Problem5 />
+        </Route>
+        <Route exact path="/meetings">
+          <Meetings />
+        </Route>
 
-          {/* <Route exact path="/login">
+        <Route exact path="/onboarding">
+          <Onboard />
+        </Route>
+
+        {/* <Route exact path="/login">
             <div className="App">
              <Login /> 
             </div>
           </Route> */}
-        </Switch>
-
+      </Switch>
     </Router>
-
-    
   );
 }
 
