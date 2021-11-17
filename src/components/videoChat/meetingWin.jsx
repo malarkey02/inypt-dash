aa46988bac50c9a7e0d3126fed9138c27f029f27
Anupam Dash <anupam.dash0505@gmail.com>
2021-11-17 06:48:02 +0530

    added jitsit meet iframe (super buggy)

--- src/components/videoChat/meetingWin.jsx
+++ src/components/videoChat/meetingWin.jsx
@@ -6,7 +6,28 @@ function Meetings(props) {
   //   isPaneOpen: false,
   //   isPaneOpenLeft: false,
   // });
-  return <h1>meeting window</h1>;
+
+  function roomNameGen() {
+    // api.dispose();
+    return Math.random().toString(36).substring(2);
+  }
+  // console.log(result);
+
+  const domain = "meet.jit.si";
+  const options = {
+    roomName: roomNameGen(), //random name for now
+    width: 1200,
+    height: 800,
+    parentNode: document.querySelector("#meet-frame"),
+  };
+
+  const api = new window.JitsiMeetExternalAPI(domain, options);
+  window.onunload = () => {
+    api.executeCommand("hangup");
+    api.dispose();
+  };
+
+  return <div id="meet-frame"></div>;
 }
 
 export default Meetings;
